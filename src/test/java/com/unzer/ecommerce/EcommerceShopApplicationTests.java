f7001e02f30958a00e4f9940a6b19ef9e5727429
Keshav Jha <iamkeshav0781@gmail.com>
2026-07-17 19:06:24 +0200

    Initial Spring Boot project skeleton

---
+++ src/test/java/com/unzer/ecommerce/EcommerceShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unzer.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
